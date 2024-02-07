3829f0bd82edbaaa963d5f78a7e0e1b18aaf9460
disara7 <129250994+disara7@users.noreply.github.com>
2024-02-07 13:12:51 +0530

    project initiated

---
+++ App/shopsense/README.md
@@ -0,0 +1,3 @@
+# shopsense
+
+A new Flutter project.
---
+++ App/shopsense/android/app/src/main/kotlin/com/example/shopsense/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shopsense
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
